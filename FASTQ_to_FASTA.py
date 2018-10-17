0da495234a7372a5df365b93b1e0af49cdb9815c
Aditya Ambati <ambati@stanford.edu>
2018-10-17 11:52:51 -0700

    added argparse for argument parsing
    Argparse added with more description on what this script is accomplishing
    to be added is how to catch errors and wrap it in a function

--- FASTQ_to_FASTA.py
+++ FASTQ_to_FASTA.py
@@ -1,6 +1,12 @@
-import re, copy, gzip, os, sys, datetime,subprocess
-file =sys.argv[1]
-outfile = sys.argv[2]
+import re, copy, gzip, os, sys, datetime,subprocess, argparse
+""" This script expects a input to be a FASTQ file via the -fastq argument that will be converted to a 2 line FASTA file"""
+parser = argparse.ArgumentParser(description='Process a FASTQ file into a FASTA file to be processed by the BLAST engine')
+parser.add_argument('-fastq', required=True, help='This is required argument input a FASTQ')
+parser.add_argument('-outfile', required=False, help='a string giving absolute path, if you provide a .gz suffix, the script will write out a gz file else just a fasta file')
+args=parser.parse_args()
+print args
+file =args.fastq
+outfile = args.outfile
 
 
 line_n = 1
